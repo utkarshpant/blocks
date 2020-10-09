5f7c7fb0c3f7a3009a204a6006d171f1a6be5ce2
Utkarsh Pant <utkarsh.pant@outlook.com>
2020-10-10 02:12:23 +0530

    Rectified print_list and erase functions.

--- telemetry/examples/list_example.cpp
+++ telemetry/examples/list_example.cpp
@@ -3,9 +3,31 @@
 
 int main() {
     List<int> list;
+    List<int> list2;  
     list.push_back(10);
     list.push_back(20);
     list.push_back(30);
+    list.push_back(40);
+    list.push_back(50);
+
+    std::cout << "Printing List 1:\n";
+    try{
+        list.print_list(",");
+    } catch(EmptyContainerException e) {
+        std::cout << e.what() << std::endl;
+    }
+    std::cout << "ID List 2:\t" << list2.getID() << std::endl;
+
+    try{
+        list.erase(3);
+    } catch (OutOfBoundsException e) {
+        std::cout << e.what() << std::endl;
+    } catch(EmptyContainerException e) {
+        std::cout << e.what() << std::endl;
+    }
 
     list.print_list();
+
+
+    // list.print_list();
 }
